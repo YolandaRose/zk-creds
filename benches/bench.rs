99d21dd981265e09f3765efd404987976c02047f
Yolanda <liuyolanda@163.com>
2026-04-14 16:49:13 +0800

    修复签名细节

--- benches/bench.rs
+++ benches/bench.rs
@@ -19,9 +19,9 @@ use util::new_size_file as setup; // 需要设置日志证明大小到CSV
 
 criterion_group!(
     benches,
-    bench_passport,
+    // bench_passport,
     // bench_student_id,
-    // bench_employee_id,
+    bench_employee_id,
     // bench_joint_student_employee,
     // bench_joint_passport_student,
     // bench_joint_passport_employee,
